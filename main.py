4e531ff9c75417fd4d6153323e4d152a65a251d8
sunbro1337 <kirill.tulyakov231@gmail.com>
2021-10-28 22:58:37 +0300

    tests of testrail api

---
+++ main.py
@@ -0,0 +1,9 @@
+from tesrail_test  import get_case_request
+
+if __name__ == '__main__':
+    get_case_request(
+        client_url='',
+        client_user='',
+        client_password='',
+        case_id=''
+    )
