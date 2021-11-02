fe94af82a24a3a204f62d1395f2a5031b846e580
sunbro1337 <kirill.tulyakov231@gmail.com>
2021-11-03 01:05:23 +0300

    classes tests

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from testrail.methods_testrail import GetMethod
+from testrail.filters import GetCaseFilter
 import tesrail_test
 
 if __name__ == '__main__':
@@ -9,6 +10,7 @@ if __name__ == '__main__':
     )
     tesrail_test.get_request(
         client,
-        method=GetMethod.GET_CASE,
-        case_id='10640173'
+        method=GetMethod().get_case_more(1, 2,
+                                         GetCaseFilter().created_after("01"),
+                                         GetCaseFilter().created_before("02"))
     )
--- testrail/methods_testrail.py
+++ testrail/methods_testrail.py
@@ -7,20 +7,18 @@ class GetMethod:
         self.GET_CASES = "get_cases"
         self.GET_HISTORY_FOR_CASE = "get_history_for_case"
 
-    def get_case(self, case_id=1, *_filters: GetCaseFilter):
+    def get_case(self, case_id=1):
+        return f"{self.GET_CASE}/{case_id}"
+
+    def get_case_more(self, project_id: int, suite_id: int, *_filters: GetCaseFilter):
         _filters_str = ''
         if _filters:
             for i in _filters:
                 _filters_str = _filters_str + str(i)
-        return f"{self.GET_CASE}/{case_id}{_filters_str}"
-
-    def get_case_more(self, project_id, suite_id):
-        return f"{self.GET_CASES}/{project_id}&suite_id={suite_id}"
+        return f"{self.GET_CASES}/{project_id}&suite_id={suite_id}{_filters_str}"
 
     def get_history_for_case(self, case_id):
         return f"{self.GET_HISTORY_FOR_CASE}/{case_id}"
 
 
-get_method = GetMethod()
-get_case_filter = GetCaseFilter()
-print(get_method.get_case(1, get_case_filter.created_after("01"), get_case_filter.created_before("02")))
+print(GetMethod().get_case_more(1, 2, GetCaseFilter().created_after("01"), GetCaseFilter().created_before("02")))
