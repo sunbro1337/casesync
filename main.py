c77f0f2f7a9e044a90ad4783831d9cba0596db0a
sunbro1337 <kirill.tulyakov231@gmail.com>
2021-11-09 19:17:40 +0300

    add parse_template_config

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import sys
+from xml.etree import ElementTree
+
 from testrail.requests import *
 from testrail.parameters import GetCaseMoreParameter
 from testrail.cases_methods import GetMethod, PostMethod
@@ -6,17 +9,30 @@ from testrail.cases_methods import GetMethod, PostMethod
 WOWSC_PROJECT_ID = 21
 
 
+def parse_template_config(tag):
+    result = None
+    for event, elem in ElementTree.iterparse("template_config.xml"):
+        if elem.tag == tag:
+            result = elem.text
+            elem.clear()
+    if result:
+        return str(result)
+    else:
+        print("Not found")
+        sys.exit()
+
+
 if __name__ == '__main__':
     client = auth_client(
-        url='',
-        user='',
-        password=''
+        url=parse_template_config("url"),
+        user=parse_template_config("user"),
+        password=parse_template_config("password")
     )
 
-    #get_request(
-    #    client=client,
-    #    method=GetMethod().get_case(6611042)
-    #)
+    get_request(
+        client=client,
+        method=GetMethod().get_case(6611042)
+    )
 
     get_request(
         client=client,
