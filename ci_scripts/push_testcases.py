d9638591acff9841b3c369e686fedc72ea8dfefe
sunbro1337 <kirill.tulyakov231@gmail.com>
2022-04-28 18:58:43 +0300

    push_testcases changes

--- ci_scripts/push_testcases.py
+++ ci_scripts/push_testcases.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 import os
 
@@ -11,6 +12,12 @@ from src.testrail.testrail_requests import *
 
 # To install python libs on powershell: py -3.10 -m pip install package_name
 
+argparser = argparse.ArgumentParser()
+argparser.add_argument("--url")
+argparser.add_argument("--usr")
+argparser.add_argument("--apikey")
+args = argparser.parse_args()
+
 logger = create_logger(
     name='build',
     logger_lvl=logging.DEBUG,
@@ -20,7 +27,6 @@ logger = create_logger(
 CLIENT_CONFIG_PATH = os.path.join('..', 'configs', 'client_config.yaml')
 diff_data_path = os.path.join('..', diff_file_name)
 CLIENT_CONFIG_DICT = read_yaml(CLIENT_CONFIG_PATH)
-print(CLIENT_CONFIG_DICT)
 logger.debug(f"CLIENT_CONFIG_PATH: {CLIENT_CONFIG_PATH}")
 logger.debug(f"diff_data_path: {diff_data_path}")
 logger.debug(f"CLIENT_CONFIG_DICT: {CLIENT_CONFIG_DICT}")
@@ -31,10 +37,15 @@ diff_data = read_json(diff_data_path)
 logger.debug(f"Diff data: {diff_data}")
 
 logger.info("Start auth gitlab client on testrail")
+# client = auth_client(
+#     url=CLIENT_CONFIG_DICT['testrail']['url'],
+#     user=CLIENT_CONFIG_DICT['testrail']['username'] + "@wargaming.net",
+#     password=CLIENT_CONFIG_DICT['testrail']['apikey'],
+# )
 client = auth_client(
-    url=CLIENT_CONFIG_DICT['testrail']['url'],
-    user=CLIENT_CONFIG_DICT['testrail']['username'] + "@wargaming.net",
-    password=CLIENT_CONFIG_DICT['testrail']['apikey'],
+    url=args.url,
+    user=args.usr + "@wargaming.net",
+    password=args.apikey,
 )
 assert client
 logger.info("Auth is completed")
@@ -54,8 +65,8 @@ logger.info('Workspace is created')
 logger.info("Start pushing changes on testrail")
 for i in diff_data:
     case_path = diff_data[i]
-    if os.path.isfile(case_path) and '.yaml' in case_path:
+    if os.path.isfile(case_path) and '.yaml' in case_path and 'test_cases' in case_path:
         logger.info(f"Pushing changes from {case_path}...")
-        # workspace.add_case(case_path)
+        workspace.add_case(case_path)
         logger.info("Success")
 logger.info("Pushing changes is completed")
